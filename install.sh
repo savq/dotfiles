9e3d313ebfbed48e79327450d9aa4d776076adc9
Sergio Alejandro Vargas <savargasqu@unal.edu.co>
2020-03-23 01:23:10 -0500

    Simplified install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,28 @@
-#!usr/bin/env zsh
+#!/bin/zsh
+
+echo "Running install.sh"
+#Symlinks
+ln -s ~/.dotfiles/alacritty/  ~/.config/alacritty/
+ln -s ~/.dotfiles/git/        ~/.config/git/
+ln -s ~/.dotfiles/nvim/       ~/.config/nvim/
+
+ln -s ~/.dotfiles/zsh/   ~/.config/zsh/
+ln -s ~/.dotfiles/zshenv ~/.zshenv
+
+ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
+echo "Created symlinks"
 
 #Install Homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 if [[$? -eq 0]]; then
   success = true
-  echo "Homebrew was installed correctly\nRunning 'brew bundle'"
   brew bundle 
+
+  raco pkg install --auto xrepl #Better repl, with dependencies (There's a lot).
+  npm install -g p5-manager
 else
   success = false
-  echo "Homebrew installation FAILED!"
 fi
-
 
 #Install Vim-plug for Neovim
 if [["$success" = true]]; then
@@ -20,22 +32,4 @@ if [["$success" = true]]; then
     echo "vim-plug was installed correctly"
   fi
 fi
-
-#Symlinks
-if [["$success" = true]]; then
-  ln -s ~/.dotfiles/alacritty/  ~/.config/alacritty/
-  ln -s ~/.dotfiles/git/  ~/.config/git/
-  ln -s ~/.dotfiles/nvim/  ~/.config/nvim/
-
-  ln -s ~/.dotfiles/zsh/  ~/.config/zsh/
-  ln -s ~/.dotfiles/zshenv ~/.zshenv
-
-  ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
-  echo "Created symlinks"
-fi
-
-if [["$success" = true]]; then
-  #raco pkg install --auto xrepl #Better repl, with dependencies (There's a lot).
-  npm install -g p5-manager
-if 
 
