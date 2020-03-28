bfc0d286078b4055f3c1dc680360cf6b67b9ee47
Sergio Alejandro Vargas <savargasqu@unal.edu.co>
2020-03-28 01:27:52 -0500

    simplified install.sh even more

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/bin/zsh
 
-echo "Running install.sh"
 #Symlinks
 ln -s ~/.dotfiles/alacritty/  ~/.config/alacritty/
 ln -s ~/.dotfiles/git/        ~/.config/git/
@@ -13,23 +12,23 @@ ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
 echo "Created symlinks"
 
 #Install Homebrew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-if [[$? -eq 0]]; then
-  success = true
-  brew bundle 
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" \
+    #Install programs listed on the Brewfile
+    && brew bundle \
+    #Install Vim-plug for Neovim
+    && curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim \
+    #Better repl, with dependencies (There's a lot).
+    && raco pkg install --auto xrepl \
+    #For p5.js
+    | npm install -g p5-manager
 
-  raco pkg install --auto xrepl #Better repl, with dependencies (There's a lot).
-  npm install -g p5-manager
-else
-  success = false
-fi
-
-#Install Vim-plug for Neovim
-if [["$success" = true]]; then
-  curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-  if [[$? -eq 0]]; then
-    echo "vim-plug was installed correctly"
-  fi
-fi
+# Initialize base conda environment
+conda init zsh \
+    # Install and setup Jupyter
+    && conda install -c conda-forge jupyterlab \
+    # Theme extension
+    && conda install jupyterthemes \
+    # Gruvbox dark theme w/ Vim color support
+    && jt -t onedork -vim 
 
