8e7ff7501f0c80fb5e98c1c68b67e1655bb59427
Sergio Alejandro Vargas <savargasqu@unal.edu.co>
2020-08-31 15:26:22 -0500

    Tweak terminal colors

--- julia-config/startup.jl
+++ julia-config/startup.jl
@@ -1,26 +1,26 @@
-using Crayons
-import OhMyREPL: Passes.SyntaxHighlighter
+#=
+#using Crayons
+#import OhMyREPL: Passes.SyntaxHighlighter
 
-### Revise
-#try
-#    using Revise
-#catch e
-#    @warn(e.msg)
-#end
+### Revise (slow)
+try
+    using Revise
+catch e
+    @warn(e.msg)
+end
 
-
-### OhMyREPL
+### OhMyREPL (ridiculously slow)
 atreplinit() do repl
     try
         @eval using OhMyREPL
         OhMyREPL.input_prompt!(">")
         SyntaxHighlighter.add!("savq", create_savq_colorscheme())
+        OhMyREPL.enable_pass!("RainbowBrackets", enable=false)
         OhMyREPL.colorscheme!("savq")
     catch e
         @warn "error while importing OhMyREPL" e
     end
 end
-
 
 #Colorscheme for OhMyREPL
 function create_savq_colorscheme()
@@ -30,13 +30,14 @@ function create_savq_colorscheme()
         Crayon(foreground = :green),        # string
         Crayon(foreground = :default),      # call
         Crayon(foreground = :light_blue),   # op
-        Crayon(foreground = :yellow),       # keyword
+        Crayon(foreground = :light_yellow), # keyword (orange)
         Crayon(foreground = :default),      # text
         Crayon(foreground = :default),      # function_def
         Crayon(foreground = :red),          # error
         Crayon(foreground = :light_blue),   # argdef
-        Crayon(foreground = :light_yellow), # _macro
+        Crayon(foreground = :yellow),       # _macro
         Crayon(foreground = :light_magenta) # number
     )
 end
+=#
 
