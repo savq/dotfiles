dcb51bd07c2311b2e6642b3147b6409354b5cf45
Sergio Alejandro Vargas <savargasqu@unal.edu.co>
2020-09-16 10:46:35 -0500

    Update Brefile, install.sh

--- Brewfile
+++ Brewfile
@@ -4,6 +4,7 @@ tap "homebrew/cask"
 tap "homebrew/cask-fonts"
 tap "homebrew/core"
 
+brew "arduino-cli"
 brew "ccls"           # C language server
 brew "deno"
 brew "node"
@@ -12,7 +13,7 @@ brew "python"         # >= 3.8
 brew "rustup-init"    # Rust toolchain installer
 cask "julia"
 
-#### Terminal
+#### TUI
 brew "neovim", args: ["HEAD"] # nvim 0.5
 brew "tmux"
 brew "zsh-syntax-highlighting"
@@ -23,20 +24,20 @@ brew "pandoc"
 cask "mactex-no-gui"
 
 #### GUI
-#cask "arduino"
-#cask "balenaetcher"
+cask "balenaetcher"
 cask "firefox"
 cask "font-fira-code"
 cask "font-ibm-plex"
 #cask "ghdl"
 #cask "gtkwave"
-cask "iina"       # Media player
-cask "itsycal"    # Calendar
+cask "iina"          # media player
+cask "itsycal"       # calendar in menu bar
 cask "protonvpn"
 cask "qbittorrent"
+cask "rectangle"     # like a tiling wm
 cask "spotify"
-cask "spotmenu"   # Spotify status bar
-#cask "supercollider"
+cask "spotmenu"      # spotify in menu bar
+cask "supercollider"
 cask "zotero"
 #cask "android-file-transfer"
 #cask "google-backup-and-sync"
--- install.sh
+++ install.sh
@@ -1,16 +1,15 @@
 #!/bin/zsh
 
 # Symlinks
-ln -s ~/.dotfiles/alacritty  ~/.config/alacritty
-ln -s ~/.dotfiles/git        ~/.config/git
-ln -s ~/.dotfiles/nvim       ~/.config/nvim
-ln -s ~/.dotfiles/zathura    ~/.config/zathura
-ln -s ~/.dotfiles/zsh        ~/.config/zsh
+mkdir ~/.config
+ln -s ~/.dotfiles/alacritty ~/.config/
+ln -s ~/.dotfiles/git       ~/.config/
+ln -s ~/.dotfiles/nvim      ~/.config/
+ln -s ~/.dotfiles/zsh       ~/.config/
 
-ln -s ~/.dotfiles/nvim/vimrc  ~/.vimrc
-ln -s ~/.dotfiles/zshenv      ~/.zshenv
-ln -s ~/.dotfiles/tmux.conf   ~/.tmux.conf
-echo "Created symlinks"
+ln -s ~/.dotfiles/zshenv     ~/.zshenv
+ln -s ~/.dotfiles/tmux.conf  ~/.tmux.conf
+echo 'Created symlinks'
 
 # Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)" &&
@@ -20,7 +19,3 @@ echo "Created symlinks"
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-# Python/Jupyter
-pip3 install jupyter &&
-  pip3 install jupyterthemes &&
-  jt -t onedork -vim
