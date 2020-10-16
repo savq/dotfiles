a3a709a9cda339a0d93aa32d51b6a7d731059454
Sergio Alejandro Vargas <savargasqu@unal.edu.co>
2020-10-16 12:33:41 -0500

    Remove julia/config

--- julia/packages.jl
+++
@@ -1,9 +0,0 @@
-# List of packages to install when installing Julia
-using Pkg
-
-Pkg.add("CSV")
-Pkg.add("DataFrames")
-Pkg.add("Plots")
-Pkg.add("Pluto")
-Pkg.add("Revise")
-Pkg.add("LanguageServer")
