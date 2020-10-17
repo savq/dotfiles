69ac630ad7e361be2d58d2379d3b18a495ca2665
Sergio Alejandro Vargas <savargasqu@unal.edu.co>
2020-10-17 16:39:48 -0500

    Finish porting config to Lua. Reorganise a little

--- nvim/init.vim
+++ nvim/init.vim
@@ -3,17 +3,3 @@
 runtime vimrc
 runtime lsp.vim
 lua require('config')
-
-"" LIGHTLINE
-let g:lightline = {
-\   'colorscheme': 'ayu_mirage',
-\   'active':{'left':  [['mode', 'paste'],
-\                       ['gitbranch', 'readonly', 'filename', 'modified']],
-\             'right': [['percent', 'lineinfo'],
-\                       ['spell', 'filetype', 'fileencoding', 'fileformat']]}}
-
-" wiki.vim function
-function CreateLinkNames(text) abort
-  return strftime("%Y%m%dT%H%M-") . substitute(tolower(a:text), '\s\+', '-', 'g')
-endfunction
-
--- nvim/lua/config.lua
+++ nvim/lua/config.lua
@@ -1,7 +1,7 @@
 -- Useful aliases
 local g = vim.g
 local cmd = vim.cmd
-function noremapsl(str) vim.cmd('noremap <silent><leader>' .. str) end
+function map(str) vim.cmd('noremap <silent><leader>' .. str) end
 
 --- PLUGINS
 cmd 'packadd paq-nvim'
@@ -13,11 +13,12 @@ paq 'nvim-lua/completion-nvim'
 paq 'nvim-lua/diagnostic-nvim'
 paq 'tjdevries/lsp_extensions.nvim'
 
-paq 'JuliaEditorSupport/julia-vim'
 paq 'lervag/vimtex'
 paq 'lervag/wiki.vim'
 paq 'vim-pandoc/vim-pandoc'
 paq 'vim-pandoc/vim-pandoc-syntax'
+
+paq 'JuliaEditorSupport/julia-vim'
 
 paq 'ayu-theme/ayu-vim'
 paq 'itchyny/lightline.vim'
@@ -25,23 +26,8 @@ paq 'junegunn/vim-easy-align'
 paq{'norcalli/nvim-colorizer.lua', opt=true} --Highlight hex and rgb colors
 
 
--- Theme: Ayu mirage
-vim.o.termguicolors = true
-g.ayucolor = 'mirage'
-cmd 'colorscheme ayu'
-cmd 'hi Comment gui=italic'
-
-
--- Julia
-g.latex_to_unicode_tab = 0
-g.latex_to_unicode_auto = 1
-cmd 'noremap <expr> <F7> LaTeXtoUnicode#Toggle()'
-noremapsl 'j :!julia %<cr>'
-
-
 -- Vimtex
-g.tex_flavor = 'lualatex' --'xelatex'
-g.vimtex_quickfix_mode = 2 -- Open _only_ if there are errors
+g.tex_flavor = 'lualatex'
 
 
 -- Wiki.vim
@@ -49,14 +35,47 @@ g.wiki_root = '~/Documents/notes/'
 g.wiki_filetypes = {'md'}
 g.wiki_link_target_type = 'md'
 g.wiki_map_link_create = 'CreateLinkNames'
---TODO write CreateLinkNames() in Lua and pass it to vimscript
+
+function createlinks(text)
+    return os.date('%Y%m%dT%H%M-') .. text:lower():gsub('[%s.]+', '-')
+end
+cmd [[
+    function CreateLinkNames(text) abort
+        return v:lua.createlinks(a:text)
+    endfunction
+]]
+
+
+-- Julia
+g.latex_to_unicode_tab = 0
+g.latex_to_unicode_auto = 1
+map 'j :!julia %<cr>'
 
 
 -- Pandoc markdown
-cmd 'let pandoc#spell#enabled = 0'
-cmd 'let pandoc#syntax#conceal#use = 0'
-cmd 'let g:pandoc#syntax#conceal#urls = 1'
+g['pandoc#spell#enabled'] = 0
+g['pandoc#syntax#conceal#use'] = 1
+g['pandoc#syntax#conceal#urls'] = 1
 cmd 'au BufNewFile,BufRead *.md set nowrap'
+
+
+-- Theme: Ayu mirage
+cmd 'colorscheme ayu'
+cmd 'autocmd ColorScheme * hi Comment gui=italic'
+g.ayucolor = 'mirage'
+vim.o.termguicolors = true
+
+
+-- Lightline
+g.lightline = {
+    colorscheme = 'ayu_mirage',
+    active = {
+        left = {{'mode', 'paste'},
+                {'readonly', 'filename', 'modified'}},
+        right= {{'percent', 'lineinfo'},
+                {'spell', 'filetype', 'fileencoding', 'fileformat'}}
+    }
+}
 
 
 -- Netrw
@@ -65,17 +84,18 @@ g.netrw_liststyle = 3  --tree style listing
 g.netrw_dirhistmax = 0 --no netrw history
 
 
--- SL MAPPINGS
 
-noremapsl 't :sp\\|:te<cr>'           -- Open terminal
-noremapsl 'rc :e ~/.config/nvim<cr>'  -- Open config directory
+-- MAPPINGS
+
+map 't :sp\\|:te<cr>'           -- Open terminal
+map 'rc :e ~/.config/nvim<cr>'  -- Open config directory
 -- Print date & time
-noremapsl('d "= "' .. vim.fn.strftime('%Y-%m-%d %T') .. '"<cr>p')
+map('d "= "' .. vim.fn.strftime('%Y-%m-%d %T') .. '"<cr>p')
 
 -- Spelling
-noremapsl 'z b1z=e'                -- Correct previous word
-noremapsl 'x 1z=1'                 -- Correct current word
-noremapsl 's :lua CycleLang()<cr>' -- Change spelling language
+map 'z b1z=e'                -- Correct previous word
+map 'x 1z=1'                 -- Correct current word
+map 's :lua CycleLang()<cr>' -- Change spelling language
 
 do -- Lua version of this solution: stackoverflow.com/a/12006781
     local spl = {i = 1, langs = {'', 'en', 'es', 'de'}}
