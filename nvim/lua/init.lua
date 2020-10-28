402972737e7005dfc445f75e2d1ef813209e1a5b
Sergio Alejandro Vargas <savargasqu+git@unal.edu.co>
2020-10-28 17:58:54 -0500

    Add "zen mode". Update some wiki & pandoc settings
    * Add more aliases for vim option tables
    * Change mappings for spell correction

--- nvim/lua/init.lua
+++ nvim/lua/init.lua
@@ -1,5 +1,5 @@
 -- Useful aliases
-local g = vim.g
+local g, o, wo, bo = vim.g, vim.o, vim.wo, vim.bo
 local cmd = vim.cmd
 local function map(str) vim.cmd('noremap <silent><leader>' .. str) end
 
@@ -31,14 +31,14 @@ g.tex_flavor = 'lualatex'
 
 
 -- Wiki.vim
-g.wiki_root = '~/Documents/notes/'
+g.wiki_root = '~/Documents/wiki/'
 g.wiki_filetypes = {'md'}
 g.wiki_link_target_type = 'md'
 g.wiki_map_link_create = 'CreateLinkNames'
 function createlinks(txt)
     return os.date('%Y%m%dT%H%M-') .. txt:lower():gsub('[%s.]+', '-')
 end
-cmd [[function CreateLinkNames(txt) abort
+cmd [[function! CreateLinkNames(txt) abort
         return v:lua.createlinks(a:txt)
       endfunction]]
 
@@ -46,6 +46,7 @@ cmd [[function CreateLinkNames(txt) abort
 -- Julia
 g.latex_to_unicode_tab = 0
 g.latex_to_unicode_auto = 1
+g.latex_to_unicode_file_types = {'julia', 'lisp', 'pandoc'}
 map 'j :!julia %<cr>'
 
 
@@ -53,6 +54,7 @@ map 'j :!julia %<cr>'
 g['pandoc#spell#enabled'] = 0
 g['pandoc#syntax#conceal#use'] = 1
 g['pandoc#syntax#conceal#urls'] = 1
+g['pandoc#folding#fdc'] = 0
 cmd 'au BufNewFile,BufRead *.md set nowrap'
 
 
@@ -60,7 +62,7 @@ cmd 'au BufNewFile,BufRead *.md set nowrap'
 cmd 'colorscheme ayu'
 cmd 'autocmd ColorScheme * hi Comment gui=italic'
 g.ayucolor = 'mirage'
-vim.o.termguicolors = true
+o.termguicolors = true
 
 
 -- Lightline
@@ -73,25 +75,34 @@ g.lightline = {
 }
 
 
---- OTHER MAPPINGS
-
-map 'l :luafile %<cr>'          -- Source lua file
-map 't :sp\\|:te<cr>'           -- Open terminal
-map 'rc :e ~/.config/nvim<cr>'  -- Open config directory
-map('d "= "' .. os.date('%Y-%m-%d T %T') .. '"<cr>p') -- Print date & time
-
 -- Spelling
-map 'z b1z=e'                -- Correct previous word
-map 'x 1z=1'                 -- Correct current word
+map 'x b1z=e'                -- Correct previous word
+map 'c 1z=1'                 -- Correct current word
 map 's :lua cyclelang()<cr>' -- Change spelling language
-
 do -- Lua version of this solution: stackoverflow.com/a/12006781
     local i = 1
     local langs = {'', 'en', 'es', 'de'}
     function cyclelang()
         i = (i % #langs) + 1          -- update index
-        vim.bo.spelllang = langs[i]   -- change spelllang
-        vim.wo.spell = langs[i] ~= '' -- if empty then nospell
+        bo.spelllang = langs[i]   -- change spelllang
+        wo.spell = langs[i] ~= '' -- if empty then nospell
     end
 end
+
+
+-- Poor man's Zen mode
+map 'z :lua togglezen()<cr>'
+function togglezen()
+    o.laststatus      = o.laststatus == 2 and 0 or 2
+    o.ruler           = not o.ruler
+    wo.number         = not wo.number
+    wo.relativenumber = not wo.relativenumber
+end
+
+
+--- Other mappings
+map 'l :luafile %<cr>'          -- Source lua file
+map 't :sp\\|:te<cr>'           -- Open terminal
+map 'rc :e ~/.config/nvim<cr>'  -- Open config directory
+map('d "= "' .. os.date('%Y-%m-%d T %T') .. '"<cr>p') -- Print date & time
 
