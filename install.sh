1e62d43e83698e2e39ff62cf655f76b915416394
Sergio Alejandro Vargas <savargasqu+git@unal.edu.co>
2020-11-22 15:36:55 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,10 @@
-#!/bin/zsh
+#!/bin/sh
 
 # Install Homebrew and download formulas listed on the Brefile
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)" &&
-  brew bundle || { echo 'brew failed' ; exit 1; }
+curl -fsSL 'https://raw.githubusercontent.com/Homebrew/install/master/install.sh' &&
+  brew bundle || { echo 'brew failed'; exit 1; }
 
 # Create symlinks
-ln -s ~/.config/julia        ~/.julia/config
 ln -s ~/.config/nvim/vimrc   ~/.vimrc
 ln -s ~/.config/zsh/.zshenv  ~/.zshenv
 echo 'Created symlinks'
@@ -13,4 +12,3 @@ echo 'Created symlinks'
 # Install Paq :)
 git clone https://github.com/savq/paq-nvim.git \
     "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/pack/paqs/opt/paq-nvim
-
