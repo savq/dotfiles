252fb0158c2c1df6d81b90bdb9608bcd3ee93d3d
Sergio Alejandro Vargas <savargasqu+git@unal.edu.co>
2020-12-14 08:30:55 -0500

    Add plenary

--- nvim/lua/packages.lua
+++ nvim/lua/packages.lua
@@ -3,10 +3,13 @@ local Paq = require'paq-nvim'
 local paq = Paq.paq
 paq{'savq/paq-nvim', opt=true}
 
+require('plenary.reload').reload_module 'paq-nvim'
+
 paq 'neovim/nvim-lspconfig'
 paq 'nvim-lua/completion-nvim'
 paq 'nvim-lua/lsp_extensions.nvim'
 paq 'nvim-treesitter/nvim-treesitter'
+paq 'nvim-lua/plenary.nvim'
 
 paq 'rust-lang/rust.vim'
 paq 'kylelaker/riscv.vim'
@@ -14,18 +17,17 @@ paq 'JuliaEditorSupport/julia-vim'
 
 paq 'lervag/vimtex'
 paq 'lervag/wiki.vim'
---paq 'vim-pandoc/vim-pandoc'
+paq 'vim-pandoc/vim-pandoc'
 paq 'vim-pandoc/vim-pandoc-syntax'
 
 paq 'ayu-theme/ayu-vim'
---paq 'itchyny/lightline.vim'
-paq{'norcalli/nvim-colorizer.lua', opt=true} --Highlight hex and rgb colors
+paq 'itchyny/lightline.vim'
+paq{'norcalli/nvim-colorizer.lua', opt=true}
 paq 'rktjmp/lush.nvim'
-paq 'savq/lush-template' --Prevent template from being deleted
 
 paq 'junegunn/vim-easy-align'
 paq 'mechatroner/rainbow_csv'
 
 --Paq.install()
 Paq.update()
-Paq.clean()
+--Paq.clean()
