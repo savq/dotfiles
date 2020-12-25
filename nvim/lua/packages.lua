b4b940f96c7474090a0f6114701b13a403d16037
Sergio Alejandro Vargas <savargasqu+git@unal.edu.co>
2020-12-25 14:52:31 -0500

    Add conjure. Add description to packages file

--- nvim/lua/packages.lua
+++ nvim/lua/packages.lua
@@ -1,19 +1,25 @@
-vim.cmd 'packadd paq-nvim'
-local Paq = require'paq-nvim'
-local paq = Paq.paq
-paq{'savq/paq-nvim', opt=true}
+--  NOTE: Paq doesn't need to be loaded at startup,
+--  packages are already in `runtimepath`.
+--  This file is only loaded when needed, like so:
+--    :lua require 'packages'
 
-require('plenary.reload').reload_module 'paq-nvim'
+vim.cmd 'packadd paq-nvim'
+local Paq = require 'paq-nvim'
+local paq = Paq.paq
+require('plenary.reload').reload_module 'paq-nvim' -- Refresh list of packages everytime file is loaded
+
+paq{'savq/paq-nvim', opt=true}
 
 paq 'neovim/nvim-lspconfig'
 paq 'nvim-lua/completion-nvim'
 paq 'nvim-lua/lsp_extensions.nvim'
-paq 'nvim-treesitter/nvim-treesitter'
 paq 'nvim-lua/plenary.nvim'
+paq 'nvim-treesitter/nvim-treesitter'
 
 paq 'rust-lang/rust.vim'
 paq 'kylelaker/riscv.vim'
 paq 'JuliaEditorSupport/julia-vim'
+paq 'olical/conjure'
 
 paq 'lervag/vimtex'
 paq 'lervag/wiki.vim'
@@ -28,6 +34,6 @@ paq 'rktjmp/lush.nvim'
 paq 'junegunn/vim-easy-align'
 paq 'mechatroner/rainbow_csv'
 
---Paq.install()
+Paq.install()
 Paq.update()
 --Paq.clean()
