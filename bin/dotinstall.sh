457842a3fe74fe001e35b8c58534038f1fb2a436
Sergio Alejandro Vargas <savargasqu+git@unal.edu.co>
2021-07-17 15:27:16 -0500

    Add theme script to change alacritty colors

--- bin/dotinstall.sh
+++ bin/dotinstall.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
+# Install Homebrew and the formulas listed in the brewfile
 curl -fsSL 'https://raw.githubusercontent.com/Homebrew/install/master/install.sh' &&
   brew bundle || { echo 'brew failed'; exit 1; }
 
-ln -s ~/.config/nvim/.vimrc   ~/.vimrc
+# Symlink stuff to $HOME
+ln -s ~/.config/nvim/vimrc   ~/.vimrc
 ln -s ~/.config/zsh/.zshenv  ~/.zshenv
+
+# Symlink my shell scripts
+ln -s ~/.config/bin/* /usr/local/bin/
+
 echo 'Created symlinks'
 
