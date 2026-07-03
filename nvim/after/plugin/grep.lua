a093252b27fcaeab81325f787f50c07825de45fc
Sergio Alejandro Vargas <savargasqu+git@unal.edu.co>
2026-07-02 20:55:55 -0500

    nvim: refactor grep and repl plugins
    - Use `<leader>rg` for grep (`rg` for ripgrep).
    - Factor out `get_selection` function. Leaving it duplicated because I
      don't feel like creating more Lua files, but this makes the
      duplication more obvious.
    - Simplify `open_repl` function. The "guess filetype" functionality
      wasn't very useful.

--- nvim/after/plugin/grep.lua
+++ nvim/after/plugin/grep.lua
@@ -1,16 +1,19 @@
-function _grep(mode)
-    local start, fin
+local function get_selection(mode)
+    local first, last
     if mode == 'V' or mode == 'v' then
-        start, fin = vim.fn.getpos "'<", vim.fn.getpos "'>"
+        first, last = "'<", "'>"
     elseif mode == 'char' or mode == 'line' then
-        start, fin = vim.fn.getpos "'[", vim.fn.getpos "']"
+        first, last = "'[", "']"
     end
+    return vim.fn.getregion(vim.fn.getpos(first), vim.fn.getpos(last))
+end
 
-    local args = vim.fn.shellescape(table.concat(vim.fn.getregion(start, fin)))
-
+function _grep(mode)
+    local lines = get_selection(mode)
+    local args = vim.fn.shellescape(table.concat(lines))
     vim.cmd('silent grep! -- ' .. args)
     vim.cmd 'copen'
 end
 
-vim.keymap.set('n', '<leader>g', ':set operatorfunc=v:lua._grep<cr>g@', { silent = true })
-vim.keymap.set('v', '<leader>g', ':<c-u>call v:lua._grep(visualmode())<cr>', { silent = true })
+vim.keymap.set('n', '<leader>rg', ':set operatorfunc=v:lua._grep<cr>g@', { silent = true })
+vim.keymap.set('v', '<leader>rg', ':<c-u>call v:lua._grep(visualmode())<cr>', { silent = true })
--- nvim/after/plugin/repl.lua
+++ nvim/after/plugin/repl.lua
@@ -1,31 +1,28 @@
-_buf_repl_map = {}
+local buf_repl_map = {}
 
-local function open_repl(lang)
-    local repls = {
-        julia = { 'julia', '--project', '--startup-file=no', '-q' },
-        python = { 'python3', '-q' },
-        typescript = { 'deno', '-q' },
-        javascript = { 'deno', '-q' },
-    }
+local function open_repl(repl_cmd)
     return function()
         local buf = vim.fn.bufnr()
-        local repl = repls[lang] or { vim.opt.shell:get() }
         vim.cmd '12new'
-        _buf_repl_map[buf] = vim.fn.termopen(repl)
+        buf_repl_map[buf] = vim.fn.termopen(repl_cmd)
         vim.cmd 'wincmd k'
     end
 end
 
-function _send_to_repl(mode)
-    local start, fin
+local function get_selection(mode)
+    local first, last
     if mode == 'V' or mode == 'v' then
-        start, fin = vim.fn.getpos "'<", vim.fn.getpos "'>"
+        first, last = "'<", "'>"
     elseif mode == 'char' or mode == 'line' then
-        start, fin = vim.fn.getpos "'[", vim.fn.getpos "']"
+        first, last = "'[", "']"
     end
-    local lines = vim.fn.getregion(start, fin)
+    return vim.fn.getregion(vim.fn.getpos(first), vim.fn.getpos(last))
+end
+
+function _send_to_repl(mode)
+    local lines = get_selection(mode)
     table.insert(lines, '')
-    vim.fn.chansend(_buf_repl_map[vim.fn.bufnr()], lines) -- :h nvim_list_chans()
+    vim.fn.chansend(buf_repl_map[vim.fn.bufnr()], lines) -- :h nvim_list_chans()
 end
 
 vim.api.nvim_create_user_command('Sterminal', 'horizontal terminal', {})
@@ -33,10 +30,10 @@ vim.api.nvim_create_user_command('Vterminal', 'vertical terminal', {})
 
 vim.keymap.set('n', '<leader>e', ':set operatorfunc=v:lua._send_to_repl<cr>g@', { silent = true })
 vim.keymap.set('v', '<leader>e', ':<c-u>call v:lua._send_to_repl(visualmode())<cr>', { silent = true })
-vim.keymap.set('n', '<leader>jl', open_repl 'julia')
-vim.keymap.set('n', '<leader>py', open_repl 'python')
-vim.keymap.set('n', '<leader>sh', open_repl())
-vim.keymap.set('n', '<leader>t', open_repl(vim.opt.filetype:get()))
+vim.keymap.set('n', '<leader>jl', open_repl { 'julia', '--project', '--startup-file=no', '-q' })
+vim.keymap.set('n', '<leader>js', open_repl { 'deno', '-q' })
+vim.keymap.set('n', '<leader>py', open_repl { 'uv', 'run', 'python3', '-q' })
+vim.keymap.set('n', '<leader>sh', open_repl { vim.opt.shell:get() })
 vim.keymap.set('t', '<Esc>', [[<C-\><C-n>]])
 
 vim.api.nvim_create_autocmd('TermOpen', { pattern = '*', command = 'setlocal nospell' })
